7dfa9de8730c358c9223f3cc65d5d60fc53dda50
PRATHAM SAHU <108048412+chillbro786@users.noreply.github.com>
2023-09-16 05:39:40 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+//  file: node 01_basics/01_variables.js 
+const accountId = 1497;
+let accountEmail = "pratham@3226@gmail.com";
+var accountPassword = "94500";
+accountCity = "kharar";
+
+ let accountState ;
+
+// changing variables and see the changes ;
+
+    //  accountId= 94500;   can't be changed 
+     accountEmail = "Raman@3226@gmail.com";
+     accountPassword = "8931097913"
+    accountCity = "changigarh";
+    
+
+
+ console.log(accountId);
+ console.table([accountId ,accountEmail,accountPassword,accountCity , accountState])
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,31 @@
+//  file: node 01_basics/02_variables.js 
+
+
+"use strict";  // treat all js code as a newer version 
+ // alert(4+6)   // we are using nodejs ,not browser 
+
+      console.log(3+4)
+
+let name = "pratham "   // string 
+let age = 26 // number 
+let isLoggedIn = false// boolean
+let state ;// undifined
+
+// primitive data type  
+// number => 2 to power 53
+// bigint 
+// string => " "
+//boolean => true/false   
+// null => standalone value 
+// undefined 
+// symbol=> unique 
+
+
+
+// object 
+
+console.log(typeof "pratham")
+console.log(typeof 1)
+console.log(typeof null)
+console.log(typeof undefined)
+
