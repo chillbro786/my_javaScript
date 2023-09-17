6b84e3b17cde75b9091ca3e1eaa8571728cf6e3b
PRATHAM SAHU <108048412+chillbro786@users.noreply.github.com>
2023-09-17 02:30:03 +0000

    done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// file: node 01_basics/03_conversionOperation.js
+
+
+/*let score = "333abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number(score);
+console.log(typeof (valueNumber));
+console.log(valueNumber);*/
+
+
+
+
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueNumber = Number(score);
+// console.log(typeof (valueNumber));
+// console.log(valueNumber);
+
+
+//**********NOTE*********
+//  similarly 
+//  undefined > NaN
+//  true > 1
+//  "pratham">NaN
+
+
+
+//   let score = 1
+// let valueNumber = String (score);
+// console.log(typeof (valueNumber));
+// console.log(valueNumber);
+
